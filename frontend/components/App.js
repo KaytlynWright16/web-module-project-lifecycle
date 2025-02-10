cdc9ff4a7fc4b379693a70ecde04939ae8a7763a
Kaytlyn Wright <kaytlynwright16@gmail.com>
2025-02-10 17:17:57 -0500

    app.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    error: '',
+    todoNameInput: '',
+  }
+  onNameChange = evt => {
+    const { value } = evt.target;
+    this.setState({...this.state, todoNameInput: value});
+  }
+  fetchAllTodos = () => {
+    axios.get(URL).then(res => {
+      this.setState({...this.state, todos: res.data.data});
+    }).catch(err => {
+      this.setState({...this.state, error: err.response.data.message})
+    })
+  }
+  componentDidMount() {
+    this.fetchAllTodos();
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div id="error">Error: {this.state.error}</div>
+        <div id="todos">
+        <h2>Todos:</h2>
+        {
+          this.state.todos.map(td => {
+            return <div key={td.id}>{td.name}</div>
+          })
+        }
+        <div>Learn React</div>
+        </div>
+        <form id="todoForm">
+          <input value={this.state.todoNameInput} onChange={this.onNameChange} type="text" placeholder="Type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
